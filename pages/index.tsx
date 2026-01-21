c0e8af297473edfd5863b16cbcaf593d16c0bf13
evanfu <203357220+evanfu0110@users.noreply.github.com>
2026-01-22 02:26:37 +0800

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -83,6 +83,7 @@ export async function getServerSideProps() {
       statusPageLink: monitor?.statusPageLink,
       // @ts-ignore
       hideLatencyChart: monitor?.hideLatencyChart,
+      icon: monitor.icon,
     }
   })
 
